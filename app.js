0e8b7423328e978fab2c92a5ff823158d9b97f85
edvardakerman <ed@sagovagen.se>
2021-11-15 11:42:37 +0100

    setup cors

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(
     origin: [
       "http://localhost:3000",
       "http://127.0.0.1:3000",
-      "https://mern-catsanddogs.netlify.app",
+      "https://todo-apply.herokuapp.com",
     ],
   })
 );
